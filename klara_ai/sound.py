bc6a1a6691e3923918ed87d9f39fed4176c6a1f0
Danil <danil.kononyuk@ya.ru>
2023-01-22 12:45:33 +0800

    sample rate

--- klara_ai/sound.py
+++ klara_ai/sound.py
@@ -3,19 +3,21 @@ import pyaudio
 import wave
 
 import logging
+
 logger = logging.getLogger(__name__)
+
 
 class Sound:
     def __init__(self, config: Config):
         self.config = config
         self.p = pyaudio.PyAudio()
 
-    def play(self, sound):
+    def play(self, sound, sample_rate=24000):
         logger.info(f"Playing sound: {sound}")
         wf = wave.open(self.config.get_config("sounds")[sound], "rb")
         stream = self.p.open(
-            format=self.p.get_format_from_width(wf.getsampwidth()),
             channels=wf.getnchannels(),
+            format=sample_rate,
             rate=wf.getframerate(),
             output=True,
             output_device_index=self.config.get_config("output_device_index"),
